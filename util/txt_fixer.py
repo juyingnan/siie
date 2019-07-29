0efc42079988889b527f4e75e012c9fa030bc48a
Yingnan Ju <bunny_gg@hotmail.com>
2019-07-29 10:41:30 -0400

    camera distance * 0.57

--- util/txt_fixer.py
+++ util/txt_fixer.py
@@ -9,10 +9,17 @@ step = len(rendering_txt_list) // 100
 count = 0
 for txt_file in rendering_txt_list:
     lines = open(txt_file).read().splitlines()
-    lines[0] = '270.0' + lines[0][5:]
-    lines[1] = '180.0' + lines[1][5:]
-    lines[2] = '90.0' + lines[2][4:]
-    lines[3] = '0.0' + lines[3][5:]
+    for i in range(len(lines)):
+        numbers = lines[i].split(' ')
+        num_4 = float(numbers[3])
+        new_num_4 = num_4 * 0.57
+        numbers[3] = str(new_num_4)
+        new_line = ' '.join(numbers)
+        lines[i] = new_line
+    # lines[0] = '270.0' + lines[0][5:]
+    # lines[1] = '180.0' + lines[1][5:]
+    # lines[2] = '90.0' + lines[2][4:]
+    # lines[3] = '0.0' + lines[3][5:]
     open(txt_file, 'w').write('\n'.join(lines))
     count += 1
     if count % step == 0:
