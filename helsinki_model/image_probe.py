4997e6101859482c10138867fc82c40628a48abc
Yingnan Ju <bunny_gg@hotmail.com>
2019-08-30 22:55:45 -0400

    changed to a single probe

--- helsinki_model/image_probe.py
+++ helsinki_model/image_probe.py
@@ -10,40 +10,36 @@ import numpy as np
 # print(np.amax(img))
 # print(np.amin(img))
 
-file_root_dir = r'C:\Users\bunny\Desktop\Data_region'
+file_root_dir = r'C:\Users\bunny\Desktop\test'
 
 functions = ['depth', 'normal']
 min_value = 255
 max_value = 0
-for img_root in os.listdir(file_root_dir):
+input_img_a_list = list()
+for (dirpath, dirnames, filenames) in os.walk(file_root_dir):
+    input_img_a_list += [os.path.join(dirpath, file) for file in filenames if file.endswith('_d.png0001.png')]
 
-    if img_root in functions:
-        continue
+# for function in functions[:1]:
+#     output_img_b_list = list()
+#     for input_img in input_img_a_list:
+#         output_img = input_img.replace('.png', '_{}.png0001.png'.format(function[0]))
+#         if os.path.exists(output_img):
+#             output_img_b_list.append(output_img)
+#     # print('files: {}'.format(len(output_img_b_list)))
 
-    input_img_a_list = list()
-    for (dirpath, dirnames, filenames) in os.walk(os.path.join(file_root_dir, img_root)):
-        input_img_a_list += [os.path.join(dirpath, file) for file in filenames if
-                             (file.endswith('.png') and len(file) < 7)]
-
-    for function in functions[:1]:
-        output_img_b_list = list()
-        for input_img in input_img_a_list:
-            output_img = input_img.replace('.png', '_{}.png0001.png'.format(function[0]))
-            if os.path.exists(output_img):
-                output_img_b_list.append(output_img)
-        # print('files: {}'.format(len(output_img_b_list)))
-
-        for output_img in output_img_b_list:
-            img_b = io.imread(output_img)[..., :3]
-            _max = np.amax(img_b)
-            _min = np.amin(img_b)
-            if _min < min_value:
-                min_value = _min
-            if _max > max_value:
-                max_value = _max
-            if _min == 0:
-                print(output_img, 0)
-            if _max == 255:
-                print(output_img, 255)
+for output_img in input_img_a_list:
+    img_b = io.imread(output_img)[..., :3]
+    color_set = set(img_b.flat)
+    print(color_set)
+    _max = np.amax(img_b)
+    _min = np.amin(img_b)
+    if _min < min_value:
+        min_value = _min
+    if _max > max_value:
+        max_value = _max
+    if _min == 0:
+        print(output_img, 0)
+    if _max == 255:
+        print(output_img, 255)
 print('final max: {}'.format(max_value))
 print('final min: {}'.format(min_value))
